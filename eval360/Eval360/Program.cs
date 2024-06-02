b46c51de11dd7d107aec08d08bf3946744f2a83e
AdanAM <adan441@hotmail.com>
2024-06-02 02:02:49 -0600

    Update Program.cs

--- eval360/Eval360/Program.cs
+++ eval360/Eval360/Program.cs
@@ -392,3 +392,35 @@ app.MapDelete("/EliminarPuesto/{id:int}", async (int id, AplicationContext db) =
 
 app.Run();
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<AplicationContext>();
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
+        }
+
+        host.Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+
