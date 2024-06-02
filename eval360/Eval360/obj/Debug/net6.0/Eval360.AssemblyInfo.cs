6c2ec25d40c31255067c3f5398d5d432cf2403e4
Adanbmth <adan441@hotmail.com>
2024-06-01 20:10:39 -0600

    A lot of changes

--- eval360/Eval360/obj/Debug/net6.0/Eval360.AssemblyInfo.cs
+++ eval360/Eval360/obj/Debug/net6.0/Eval360.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eval360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755fb1e5ec5135ffe4467307c12b2ce26cddfb95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+013982ca4d52f37963b11c43bbd2dc18834308b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eval360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eval360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
